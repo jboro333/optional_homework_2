19771ff71eb6742f54ae15727fc2e34b666946a5
“Jordi” <jbg8@alumnes.udl.cat>
2017-03-03 17:44:15 +0100

    Almanac working, lets try forecast

---
+++ optional2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+
+import urllib2
+from bs4 import BeautifulSoup
+
+class Client(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+if __name__ == "__main__":
+    client = Client()
+    client.run()

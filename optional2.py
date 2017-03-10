7461941570d7e9d740761c17eb47b87d835452f0
“Jordi” <jbg8@alumnes.udl.cat>
2017-03-10 17:03:58 +0100

    Hourly added and working 2

--- optional2.py
+++ optional2.py
@@ -5,40 +5,51 @@
 import sys
 import requests
 import json
+# import xmltodict
 
 api_key = None
 
 
 class WeatherClient(object):
     """docstring for WeatherClient"""
-    url_base = "http://api.wunderground.com/api/"
-    url_service = {"forecast": "/forecast/q/CA/"}
+    url_base = 'http://api.wunderground.com/api/'
+    url_service = {"hourly": "/hourly/q/CA/"}
 
     def __init__(self, api_key):
         super(WeatherClient, self).__init__()
         self.api_key = api_key
 
-    def forecast(self, location):
+    def hourly(self, location):
         """
-        Accesses wunderground forecast information for the given location
+        Accesses wunderground hourly information for the given location
         """
         resp_format = "json"
         url = WeatherClient.url_base + api_key + \
             WeatherClient.url_service[
-                "forecast"] + location + "." + resp_format
+                "hourly"] + location + "." + resp_format
         r = requests.get(url)
-
         jsondata = json.loads(r.text)
-        print url
-        print jsondata
-        return jsondata["forecast"]
+        return jsondata["hourly_forecast"]
 
 
-def print_forecast(forecast):
+def print_hourly(hourly):
     """
     Prints an forecast received as a dict
     """
-    # print "Average on this date", forecast["date.day"]
+    # print hourly[1]["FCTTIME"]["hour"]
+    print "On this date: " + hourly[0]["FCTTIME"]["mday"] + "/" + \
+        hourly[0]["FCTTIME"]["mday"] + "/" + \
+        hourly[0]["FCTTIME"]["year"]
+    print " "
+    count = []
+    count = list(range(0, 24))
+    for x in count:
+        print "At this hour: " + hourly[x]["FCTTIME"]["hour"] + ":" + \
+            hourly[x]["FCTTIME"]["min"] + ":" + hourly[x]["FCTTIME"]["sec"]
+        print "Temperature: " + hourly[x]["temp"]["metric"] + "C"
+        print "Humidity: " + hourly[x]["humidity"] + "%"
+        print " "
+    print "End"
 
 
 if __name__ == "__main__":
@@ -49,4 +60,4 @@ if __name__ == "__main__":
             print "Must provide api key in code or cmdline arg"
 
     weatherclient = WeatherClient(api_key)
-    print_forecast(weatherclient.forecast("Lleida"))
+    print_hourly(weatherclient.hourly("Lleida"))
